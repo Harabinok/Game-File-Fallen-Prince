4fdb07c52d1cb637a97a622c2b056d20d630908a
Harabinok <87910293+Harabinok@users.noreply.github.com>
2022-04-15 17:12:22 +0600

    Add 3D graphica

---
+++ Fallen Prince/Assets/FallenPrince/GameSettings/Shaders/SimpalFilter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpalFilter : MonoBehaviour
+{
+    [SerializeField] private Shader shader;
+    private Material material;
+    private void Awake()
+    {
+        material = new Material(shader);
+    }
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, material);
+    }
+}
